93e7822d31ad795b32dd2f98d6fc85f3f6ed9dfb
Alfred Arouna <alfred+git@arouna.net>
2019-10-06 20:32:54 +0100

    Expose Flask port internaly

--- 03-swarm/stack/roles/manager/files/management/nginx/nginx_entry.sh
+++ 03-swarm/stack/roles/manager/files/management/nginx/nginx_entry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 echo "[Entrypoint-NGINX] Creating TLS Certificate"
-openssl req -subj '/CN=mdnssec.ri.mu.afrinic.net/' -x509 -days 365 -nodes -batch -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
+openssl req -subj '/CN<swarm_manager_ip_or_fqdn>/' -x509 -days 365 -nodes -batch -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
 echo "[Entrypoint-NGINX] Creating Diffie-Hellman parameters"
 openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
 exec "$@"
