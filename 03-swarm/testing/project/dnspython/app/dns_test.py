88314f90d8170b6d36d0f9111fe784fe5a8acf53
Alfred Arouna <alfred+git@arouna.net>
2019-10-08 17:45:52 +0100

    Update test code: read zone data from json file

--- 03-swarm/testing/project/dnspython/app/dns_test.py
+++ 03-swarm/testing/project/dnspython/app/dns_test.py
@@ -5,16 +5,17 @@ import logging as logger
 from urllib.parse import urlparse
 
 import os
+import sys
 
 
 logging = logger.getLogger(__name__)
 
-API_BASE = os.environ.get('API_BASE', 'mdnssec.ri.mu.afrinic.net')
+API_BASE = os.environ.get('API_BASE', 'mdnssec.ri.mu.net')
 API_PORT = os.environ.get('API_PORT', '30001')
 API_URL= 'http://' + API_BASE + ':' + API_PORT + '/'
 API_VERSION='api/v1/servers/localhost'
-API_KEY=os.environ.get('API_KEY', 'nyo8pUoxZb1AzkD')
-MEMBER_IP=os.environ.get('MEMBER_IP', '196.192.112.222')
+API_KEY=os.environ.get('API_KEY', 'nyo8pUoxZb1AzkDX')
+MEMBER_IP=os.environ.get('MEMBER_IP', '196.192.11.22')
 DS_FILES_FOLDER="data"
 headers = {}
 headers['X-API-Key'] = API_KEY
@@ -174,7 +175,16 @@ def set_master(zone, tsigkey):
                 if 'error' in data:
                     return {'status': 'error', 'msg': 'Zone '+zone+' could not be change to Master', 'data': data}
                 else:
-                    return {'status': 'ok', 'data': data}
+                    # Set SOA
+                    post_data = {
+                        "kind": "SOA-EDIT",
+                        "metadata": ["INCEPTION-INCREMENT"]
+                    }
+                    data = fetch_json(urljoin(API_URL,API_VERSION)  + '/zones/{0}/metadata'.format(zone), headers=headers, method='POST', data=post_data)
+                    if 'error' in data:
+                        return {'status': 'error', 'msg': 'Cannot set  soa for zone '+ zone +'. Error: {0}'.format(data['error']), 'data': data}
+                    else:
+                        return {'status': 'ok', 'data': str(data)}
             else:
                 return {'status': 'ok', 'data': data}
     except Exception as e:
@@ -210,7 +220,7 @@ def set_nsec3(zone):
     except Exception as e:
         return {'status': 'error', 'msg': str(e) }
 
-def get_ds(zone):
+def get_ds(zone, export=None):
     try:
         data = fetch_json(urljoin(API_URL,API_VERSION)  + '/zones/{0}/cryptokeys'.format(zone), headers=headers, method='GET')
         if 'error' in data:
@@ -221,11 +231,12 @@ def get_ds(zone):
                     dsset = key["ds"]
                     print(dsset)
                     if dsset:
-                        file = open(DS_FILES_FOLDER + "/dsset-"+zone, "w")
-                        for ds in dsset:
-                            print(ds)
-                            file.write(zone + ". IN DS " + ds + "\n")
-                        file.close()
+                        if export:
+                            file = open(DS_FILES_FOLDER + "/dsset-"+zone, "w")
+                            for ds in dsset:
+                                print(ds)
+                                file.write(zone + ". IN DS " + ds + "\n")
+                            file.close()
                         return {'status': 'ok', 'data': key["ds"]}
                     else:
                         return {'status': 'error', 'msg': 'No DS in '+ str(dsset)}
@@ -261,90 +272,147 @@ def set_soa(zone):
     except Exception as e:
         return {'status': 'error', 'msg': str(e) }
 
-"""
-dig +short @196.192.112.222  nsd.tld soa                                                                                                                           ✔  10547  22:56:14
-ns1.nsd.tld. hostmaster.nsd.tld. 2019080201 7200 3600 604800 43200
-dig +short @196.192.112.222 pdns.tld soa                                                                                                                          ✔  10548  22:56:23
-ns1.pdns.tld. hostmaster.pdns.tld. 2019080201 7200 3600 604800 43200
-dig +short @196.192.112.222 bind.tld soa                                                                                                                          ✔  10549  22:56:31
-ns1.bind.tld. hostmaster.bind.tld. 2019061102 7200 3600 604800 43200
+#print(len(sys.argv))
+data = None
+if len(sys.argv) == 1:
+    ## Slaves Mode
+    tsigkey = create_tsig("nsd_slave", "hmac-sha256", "TlNEX1RTSUdfU0VDUkVUX0tFWQo=")
+    print(tsigkey)
+    if tsigkey['status'] == 'ok' and tsigkey["data"]["id"]:
+        nsd_tld = create_slave_zone("nsd.tld", tsigkey["data"]["id"], MEMBER_IP)
+        print(nsd_tld)
+        if nsd_tld['status'] == 'ok' and nsd_tld["data"]["name"]:
+            nsd_data = check_axfr(nsd_tld["data"]["name"])
+            print(nsd_data)
 
-dig +short @196.192.112.222 -p 530 bind.tld soa
-"""
+    tsigkey = create_tsig("bind_slave", "hmac-sha256", "QklORF9UU0lHX1NFQ1JFVF9LRVkK")
+    print(tsigkey)
+    if tsigkey['status'] == 'ok' and tsigkey["data"]["id"]:
+        nsd_tld = create_slave_zone("bind.tld", tsigkey["data"]["id"], MEMBER_IP)
+        print(nsd_tld)
+        if nsd_tld['status'] == 'ok' and nsd_tld["data"]["name"]:
+            nsd_data = check_axfr(nsd_tld["data"]["name"])
+            print(nsd_data)
 
-## Slaves Mode
-tsigkey = create_tsig("nsd_slave", "hmac-sha256", "TlNEX1RTSUdfU0VDUkVUX0tFWQo=")
-print(tsigkey)
-if tsigkey['status'] == 'ok' and tsigkey["data"]["id"]:
-    nsd_tld = create_slave_zone("nsd.tld", tsigkey["data"]["id"], MEMBER_IP)
-    print(nsd_tld)
-    if nsd_tld['status'] == 'ok' and nsd_tld["data"]["name"]:
-        nsd_data = check_axfr(nsd_tld["data"]["name"])
-        print(nsd_data)
+    tsigkey = create_tsig("pdns_slave", "hmac-sha256", "cG93ZXJkbnNfc2VydmVyCg==")
+    print(tsigkey)
+    if tsigkey['status'] == 'ok' and tsigkey["data"]["id"]:
+        nsd_tld = create_slave_zone("pdns.tld", tsigkey["data"]["id"], MEMBER_IP)
+        print(nsd_tld)
+        if nsd_tld['status'] == 'ok' and nsd_tld["data"]["name"]:
+            nsd_data = check_axfr(nsd_tld["data"]["name"])
+            print(nsd_data)
 
-tsigkey = create_tsig("bind_slave", "hmac-sha256", "QklORF9UU0lHX1NFQ1JFVF9LRVkK")
-print(tsigkey)
-if tsigkey['status'] == 'ok' and tsigkey["data"]["id"]:
-    nsd_tld = create_slave_zone("bind.tld", tsigkey["data"]["id"], MEMBER_IP)
-    print(nsd_tld)
-    if nsd_tld['status'] == 'ok' and nsd_tld["data"]["name"]:
-        nsd_data = check_axfr(nsd_tld["data"]["name"])
-        print(nsd_data)
+    ## Master mode
+    tsigkey = create_tsig("nsd_master", "hmac-sha256", "bmRzcGRuc21hc3Rlcgo=")
+    print(tsigkey)
+    if tsigkey['status'] == 'ok' and tsigkey["data"]["id"]:
+        nsd_master = set_master("nsd.tld", tsigkey["data"]["id"])
+        print(nsd_master)
+        nsd_soa = set_soa("nsd.tld")
+        print(nsd_soa)
+        nsd_signed_ksk = create_cryptokeys("nsd.tld", "ksk", "rsasha512", True, 2048)
+        print(nsd_signed_ksk)
+        nsd_signed_zsk = create_cryptokeys("nsd.tld", "zsk", "rsasha512", True, 1024)
+        print(nsd_signed_zsk)
+        nsec3 = set_nsec3("nsd.tld")
+        print(nsec3)
+        ds = get_ds("nsd.tld")
+        print(ds)
 
-tsigkey = create_tsig("pdns_slave", "hmac-sha256", "cG93ZXJkbnNfc2VydmVyCg==")
-print(tsigkey)
-if tsigkey['status'] == 'ok' and tsigkey["data"]["id"]:
-    nsd_tld = create_slave_zone("pdns.tld", tsigkey["data"]["id"], MEMBER_IP)
-    print(nsd_tld)
-    if nsd_tld['status'] == 'ok' and nsd_tld["data"]["name"]:
-        nsd_data = check_axfr(nsd_tld["data"]["name"])
-        print(nsd_data)
+    tsigkey = create_tsig("bind_master", "hmac-sha256", "YmluZG1hc3Rlcgo=")
+    print(tsigkey)
+    if tsigkey['status'] == 'ok' and tsigkey["data"]["id"]:
+        bind_master = set_master("bind.tld", tsigkey["data"]["id"])
+        print(bind_master)
+        bind_soa = set_soa("bind.tld")
+        print(bind_soa)
+        bind_signed_ksk = create_cryptokeys("bind.tld", "ksk", "rsasha512", True, 2048)
+        print(bind_signed_ksk)
+        bind_signed_zsk = create_cryptokeys("bind.tld", "zsk", "rsasha512", True, 1024)
+        print(bind_signed_zsk)
+        nsec3 = set_nsec3("bind.tld")
+        print(nsec3)
+        ds = get_ds("bind.tld")
+        print(ds)
 
-## Master mode
-tsigkey = create_tsig("nsd_master", "hmac-sha256", "bmRzcGRuc21hc3Rlcgo=")
-print(tsigkey)
-if tsigkey['status'] == 'ok' and tsigkey["data"]["id"]:
-    nsd_master = set_master("nsd.tld", tsigkey["data"]["id"])
-    print(nsd_master)
-    nsd_soa = set_soa("nsd.tld")
-    print(nsd_soa)
-    nsd_signed_ksk = create_cryptokeys("nsd.tld", "ksk", "rsasha512", True, 2048)
-    print(nsd_signed_ksk)
-    nsd_signed_zsk = create_cryptokeys("nsd.tld", "zsk", "rsasha512", True, 1024)
-    print(nsd_signed_zsk)
-    nsec3 = set_nsec3("nsd.tld")
-    print(nsec3)
-    ds = get_ds("nsd.tld")
-    print(ds)
+    tsigkey = create_tsig("pdns_master", "hmac-sha256", "cGRuc3NsYXZla25vdAo=")
+    print(tsigkey)
+    if tsigkey['status'] == 'ok' and tsigkey["data"]["id"]:
+        pdns_master = set_master("pdns.tld", tsigkey["data"]["id"])
+        print(pdns_master)
+        pdns_soa = set_soa("pdns.tld")
+        print(pdns_soa)
+        pdns_signed_ksk = create_cryptokeys("pdns.tld", "ksk", "rsasha512", True, 2048)
+        print(pdns_signed_ksk)
+        pdns_signed_zsk = create_cryptokeys("pdns.tld", "zsk", "rsasha512", True, 1024)
+        print(pdns_signed_zsk)
+        nsec3 = set_nsec3("pdns.tld")
+        print(nsec3)
+        ds = get_ds("pdns.tld")
+        print(ds)
 
-tsigkey = create_tsig("bind_master", "hmac-sha256", "YmluZG1hc3Rlcgo=")
-print(tsigkey)
-if tsigkey['status'] == 'ok' and tsigkey["data"]["id"]:
-    bind_master = set_master("bind.tld", tsigkey["data"]["id"])
-    print(bind_master)
-    bind_soa = set_soa("bind.tld")
-    print(bind_soa)
-    bind_signed_ksk = create_cryptokeys("bind.tld", "ksk", "rsasha512", True, 2048)
-    print(bind_signed_ksk)
-    bind_signed_zsk = create_cryptokeys("bind.tld", "zsk", "rsasha512", True, 1024)
-    print(bind_signed_zsk)
-    nsec3 = set_nsec3("bind.tld")
-    print(nsec3)
-    ds = get_ds("bind.tld")
-    print(ds)
+elif sys.argv[1] == '-f' and len(sys.argv) == 3 and sys.argv[2]:
+    if os.path.isfile(sys.argv[2]):
+        with open(sys.argv[2], 'r') as myfile:
+            data=myfile.read()
+    else:
+        print(sys.argv[2] + " is not a file or path is not correct.")
+elif sys.argv[1] == '-d' and len(sys.argv) == 3 and sys.argv[2]:
+    #print(sys.argv[2])
+    data = sys.argv[2]
+else:
+    print("Missing parameters")
 
-tsigkey = create_tsig("pdns_master", "hmac-sha256", "cGRuc3NsYXZla25vdAo=")
-print(tsigkey)
-if tsigkey['status'] == 'ok' and tsigkey["data"]["id"]:
-    pdns_master = set_master("pdns.tld", tsigkey["data"]["id"])
-    print(pdns_master)
-    pdns_soa = set_soa("pdns.tld")
-    print(pdns_soa)
-    pdns_signed_ksk = create_cryptokeys("pdns.tld", "ksk", "rsasha512", True, 2048)
-    print(pdns_signed_ksk)
-    pdns_signed_zsk = create_cryptokeys("pdns.tld", "zsk", "rsasha512", True, 1024)
-    print(pdns_signed_zsk)
-    nsec3 = set_nsec3("pdns.tld")
-    print(nsec3)
-    ds = get_ds("pdns.tld")
-    print(ds)
+if sys.argv[1] == '-f' and len(sys.argv) == 3 and sys.argv[2] or sys.argv[1] == '-d' and len(sys.argv) == 3 and sys.argv[2]:
+    #parse data
+    if data:
+        obj = json.loads(data)
+        #print(obj)
+        for record in obj:
+            #print(record)
+            if 'tsigkeys' in record:
+                print("Creating Tsig keys...")
+                for tsigs in record['tsigkeys']:
+                    #print(tsigs )
+                    if 'out' in tsigs:
+                        print("TSIG Out: from Member DNS server to signer")
+                        tsig_out = create_tsig(tsigs['out']['name'],tsigs['out']['algo'], tsigs['out']['secret'])
+                    if 'in' in tsigs:
+                        print("TSIG In: from Signer to member DNS server")
+                        tsig_in = create_tsig(tsigs['in']['name'],tsigs['in']['algo'], tsigs['in']['secret'])
+
+                if 'zone' in record and tsig_out['status'] == 'ok' and tsig_out["data"]["id"]:
+                    print("Creating slave zone "+ record['zone'])
+                    #print(record['ns'])
+                    slave = create_slave_zone(record['zone'], tsig_out["data"]["id"], record['ns'])
+
+                    if slave['status'] == 'ok' and slave["data"]["name"]:
+                        print("Checking AXFR for zone " + slave["data"]["name"])
+                        axfr_data = check_axfr(slave["data"]["name"])
+
+                if 'zone' in record and tsig_in['status'] == 'ok' and tsig_in["data"]["id"]:
+                    print("Setting zone " + record['zone'] +" to master")
+                    master = set_master(record['zone'], tsig_in["data"]["id"])
+
+                if 'cryptokeys' in record:
+                    print("Creating DNSSEC Cryptokeys")
+                    for keys in record['cryptokeys']:
+                        #print(keys)
+                        if 'ksk' in keys:
+                            ksk_algo = keys['ksk']['algorithm']
+                            ksk_bits = keys['ksk']['bits']
+                            ksk = create_cryptokeys(record['zone'], "ksk", ksk_algo, keys['ksk']['active'], ksk_bits)
+                        if 'zsk' in keys:
+                            zsk_bits = keys['zsk']['bits']
+                            if int(zsk_bits) >= int(ksk_bits):
+                                zsk_bits = 1024
+                            zsk = create_cryptokeys(record['zone'], "zsk", ksk_algo, keys['zsk']['active'], zsk_bits)
+                print("Setting NSEC3 params for zone " +record['zone'])
+                set_nsec3(record['zone'])
+                print("Getting DS for zone "+record['zone'])
+                get_ds(record['zone'])
+
+
+
+
