ad87c2531d9f1545d4d4f71e2f68bf3f2add4187
Alfred Arouna <alfred+git@arouna.net>
2019-10-11 13:34:27 +0100

    Remove 01-compose and 03-swarm folders

--- 03-swarm/testing/project/dnspython/app/app.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import time
-if __name__ == "__main__":
-    while True:
-        time.sleep(1)
--- 03-swarm/testing/project/dnsviz/data/app.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-import time
-if __name__ == "__main__":
-    while True:
-        time.sleep(1)
